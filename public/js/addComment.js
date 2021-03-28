d57c6493debc5a99d7a6f63170f56b10e13ccc29
Gary B <grider27@gmail.com>
2021-03-28 15:59:26 -0400

    add comments to posts post login

---
+++ public/js/addComment.js
@@ -0,0 +1,28 @@
+const addComment = async (event) => {
+    event.preventDefault();
+    const comment = document.querySelector('#user-comment').value.trim();
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    //alert(userComment+" "+post_id);
+
+    if (comment) {
+        const response = await fetch('/api/comment', {
+            method: 'POST',
+            body: JSON.stringify({ comment, post_id }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+
+};
+
+document
+    .querySelector('.add-comment')
+    .addEventListener('submit', addComment);
